de0131f645ba153a21ce31a804259a6d337c2c73
arg042296 <33065642+arg042296@users.noreply.github.com>
2017-11-08 15:27:45 -0500

    Audio Converter
    uploading my version for github quiz

---
+++ AudioConverter.java
@@ -0,0 +1,25 @@
+/**
+ * @author sigri
+ * @version 1.0
+ * @created 10-okt-2017 18:06:10
+ */
+public class AudioConverter {
+
+	public File m_File;
+
+	public AudioConverter(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	public file getAudioFIle(){
+		return null;
+	}
+	
+	//public static void main(String[] args)
+	//{
+		
+	//}
+}//end AudioConverter
